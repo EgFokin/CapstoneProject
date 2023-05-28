789ed58ce4a8a5647107e94a7b3503cb3c66e4c1
Foksyamba <foks72tm@gmail.com>
2023-05-27 22:15:24 -0500

    Connect database. Create models for Menu,Booking

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(6)
+        ]
+    )
+    booking_date = models.DateTimeField()
+    def __str__(self):
+        return(self.name)
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return(self.title)
+
+
